d93031053e34b6237649613b7ccb1eac63a1ee4d
Kamania <kamaniajoseph@gmail.com>
2024-03-13 07:56:24 +0300

    Make data dynamic

---
+++ src/components/Result.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "Result"
+}
+</script>
+
+<template>
+  <div class="result">
+    <div class="title">You got sample result 1!</div>
+    <div class="desc">
+      Enter a short description here about the result.
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
