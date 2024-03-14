4ef00036c2c6d46cc0e10b48aa6b9fc7fa030e4d
Kamania <kamaniajoseph@gmail.com>
2024-03-14 08:18:58 +0300

    Make answers selectable

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="ctr">
-    <questions v-if="questionsAnswered < questions.length" :questions="questions" />
+    <questions
+        v-if="questionsAnswered < questions.length"
+        :questions="questions"
+        :questionsAnswered="questionsAnswered"
+        @question_answered="questionAnswered"
+    />
     <result v-else />
     <button type="button" class="reset-btn">Reset</button>
   </div>
@@ -19,6 +24,7 @@ export default {
   data(){
     return {
       questionsAnswered: 0,
+      totalCorrect: 0,
       questions: [
         {
           q: 'What is 2 + 2?',
@@ -94,6 +100,14 @@ export default {
           desc: "Studying has definitely paid off for you!"
         }
       ]
+    }
+  },
+  methods: {
+    questionAnswered(is_correct){
+      if (is_correct){
+        this.totalCorrect++;
+      }
+      this.questionsAnswered++;
     }
   }
 }
--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,7 +1,13 @@
 <script>
 export default {
   name: "Questions",
-  props: ["questions"]
+  props: ["questions","questionsAnswered"],
+  emits: ["question_answered"],
+  methods: {
+    selectAnswer(is_correct){
+      this.$emit("question_answered", is_correct);
+    }
+  }
 }
 </script>
 
@@ -11,10 +17,10 @@ export default {
       <div class="bar"></div>
       <div class="status">1 out of 3 questions answered</div>
     </div>
-    <div class="single-question" v-for="question in questions" :key="question.q">
+    <div class="single-question" v-for="(question, qi) in questions" :key="question.q" v-show="questionsAnswered == qi">
       <div class="question">{{ question.q }}</div>
       <div class="answers" v-for="answer in question.answers" :key="answer.text">
-        <div class="answer">{{ answer.text }}</div>
+        <div class="answer" @click.prevent="selectAnswer(answer.is_correct)">{{ answer.text }}</div>
       </div>
     </div>
   </div>
